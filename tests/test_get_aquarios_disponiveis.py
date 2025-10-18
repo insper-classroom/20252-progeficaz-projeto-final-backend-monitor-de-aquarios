cffcdb8e81e5a3f6fce181f7c0459a1f33e1e1c3
LGHARO <lgharo06@gmail.com>
2025-10-18 14:42:37 -0300

    red, test_get_aquarios_disponiveis

--- tests/test_get_aquarios_disponiveis.py
+++ tests/test_get_aquarios_disponiveis.py
@@ -9,7 +9,7 @@ def client():
         yield client
 
 @patch("api.connect_db") # aqui mockamos o connect_db criado dentro do arquivos para fingir que fazemos uma conexao
-def test_get_aquarios(mock_connect_db, client):
+def test_get_aquarios_disponiveis(mock_connect_db, client):
     # aqui teremos o mock do banco que vamos puxar e em seguida o mock da collection que vamos puxar tambem. fazemos dassa forma pois quando fizermos um find na collection nn fazemos um find no mock e sim um novo mock pra retornar as informações que queremos 
     mock_db = MagicMock()
     mock_collection = MagicMock() 
@@ -20,7 +20,7 @@ def test_get_aquarios(mock_connect_db, client):
     "andar": 1,
     "id_local": 1,
     "capacidade_cadeiras": 10,
-    "ocupado": True 
+    "ocupado": False 
   },
   {
     "id": 2,
@@ -38,7 +38,7 @@ def test_get_aquarios(mock_connect_db, client):
     mock_connect_db.return_value = mock_db
 
 		# realizamos a requisição que queremos testar mockando as operações que abordamos anteriormente aqui 
-    response = client.get("/aquarios")
+    response = client.get("/aquarios/disponiveis")
 		
     assert response.status_code == 200
     assert response.get_json() == { 'aquarios': [
@@ -48,7 +48,7 @@ def test_get_aquarios(mock_connect_db, client):
     "andar": 1,
     "id_local": 1,
     "capacidade_cadeiras": 10,
-    "ocupado": True
+    "ocupado": False
   },
   {
     "id": 2,
